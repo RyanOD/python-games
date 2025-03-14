d938e0c17af8387d9555ac6805b9dc4b2f91fad8
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-03-13 22:45:13 -0700

    Adding game assets

---
+++ frogger/levels.py
@@ -0,0 +1,23 @@
+LEVELS = [
+    [
+        [
+            ['D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+            ['D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+            ['HTL', 'HTR', 'HTM', 'HTL', 'HTR', 'HTM', 'HTL', 'HTR', 'HTM', 'HTL', 'HTR', 'HTM', 'HTL', 'HTR'],
+            ['HBL', 'HBR', 'HBM', 'HBL', 'HBR', 'HBM', 'HBL', 'HBR', 'HBM', 'HBL', 'HBR', 'HBM', 'HBL', 'HBR'],
+            ['LL', 'LM', 'LM', 'LR', 'W', 'W', 'LL', 'LM', 'LR', 'W', 'W', 'LL', 'LM', 'LR'],
+            ['W', 'W', 'T', 'T', 'W', 'T', 'T', 'W', 'W', 'T', 'T', 'W', 'T', 'T'],
+            ['LL', 'LM', 'LM', 'LM', 'LM', 'LR', 'W', 'W', 'LL', 'LM', 'LM', 'LM', 'LM', 'LR', 'W'],
+            ['W', 'LL', 'LM', 'LR', 'W', 'W', 'W', 'LL', 'LM', 'LR', 'W', 'W', 'W', 'LL', 'LM', 'LR'],
+            ['T', 'T', 'T', 'W', 'T', 'T', 'T', 'W', 'T', 'T', 'T', 'W', 'T', 'T'],
+            ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'],
+            [],
+            [],
+            [],
+            [],
+            [],
+            ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H'],
+            [],  
+        ]
+    ]
+]

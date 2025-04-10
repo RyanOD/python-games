1c8ff30a2cbacd8d11aa566f0969e08385c79c17
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-10 15:30:16 -0700

    Removed unnecessary Command code

--- frogger/sound.py
+++ frogger/sound.py
@@ -1,37 +1,24 @@
-
 import pygame
 from events import event_dispatcher
 
 class SoundHandler:
     def __init__(self):
         self.mixer = pygame.mixer.init()
+
+        # dict of all game sound files
         self.sounds = {
             'hop': pygame.mixer.Sound('assets/hop.mp3'),
             'die_road': pygame.mixer.Sound('assets/die_road.mp3')
         }
-        self.sound_commands = {
-            'hop': PlayHopSoundCommand(self.handle_sound),
-            'die_road': PlayRoadDeathSoundCommand(self.handle_sound),
-        }
+
+        # set sound volumes
         for sound in self.sounds.values():
             sound.set_volume(0.5)
 
+        # pass trigger word and method for playing sound to event_dispatcher which allows other modules to trigger sounds
         event_dispatcher.register("play_sound", self.handle_sound)
 
+    # method registered with and called from event_dispatcher
     def handle_sound(self, sound):
         if(sound in self.sounds):
             self.sounds[sound].play()
-
-class PlayHopSoundCommand:
-    def __init__(self, handle_sound):
-        self.sound_handler = handle_sound
-
-    def execute(self):
-        self.sound_handler('hop')
-
-class PlayRoadDeathSoundCommand:
-    def __init__(self, handle_sound):
-        self.sound_handler = handle_sound
-
-    def execute(self):
-        self.sound_handler('die_road')

7dfbcbaaf31638376f16c0b4d368ddbdc69972a5
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-14 20:46:42 -0500

    Added scoring display. Adjusted sizing.

--- frogger/screen.py
+++ frogger/screen.py
@@ -1,12 +1,13 @@
 import pygame
+from utils import load_data_file
 
 class Screen:
     def __init__(self):
-        self.width = 900
-        self.height = 1080
+        self.width = 750
+        self.height = 900
         self.lane_padding = 6
-        self.lane_height = 64
-        self.lane_width = 64
+        self.lane_height = 50
+        self.lane_width = 50
         self.caption = self.set_caption("Frogger Clone by Retro Clone")
         self.surface = pygame.display.set_mode((self.width, self.height))
         self.bg = self.get_bg("assets/bg.png")
@@ -23,4 +24,13 @@ class Screen:
         return pygame.image.load(bg_image).convert()
 
     def on_screen(self, sprite):
+        if sprite.rect.bottom > self.height - 50:
+            sprite.rect.bottom = self.height - 50
         return 0 < sprite.rect.centerx < self.width
+    
+    def score(self, player_score):
+        font = pygame.font.Font("assets/upheavtt.ttf", 34)
+        score_title = pygame.font.Font.render(font, "1-Up", True, (255, 255, 255))
+        score = pygame.font.Font.render(font, str(player_score), False, (255, 255, 255))
+        self.surface.blit(score_title, (100, 5))
+        self.surface.blit(score, (100, 33))

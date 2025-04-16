75f810eb37c721d660e1cebd0bad200421de0db9
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-16 17:34:52 -0500

    Adjusted x boundaries of frog home locations. Fixed points management.

--- frogger/game.py
+++ frogger/game.py
@@ -5,8 +5,10 @@ from input import InputHandler
 from screen import Screen
 from sound import SoundHandler
 from collision import CollisionHandler
+from scoring import Scoring
 from utils import *
 from events import event_dispatcher
+from debug import draw_grid
 
 class Game:
     def __init__(self):
@@ -21,83 +23,77 @@ class Game:
         self.surface = self.screen.surface
         self.input_handler = InputHandler(self.frog)
         self.collision_handler = CollisionHandler()
+        self.scoring = Scoring(self.frog)
         self.score = 0
         self.homes = [
-            {'occupied': False, 'xl': 60, 'xr': 120},
-            {'occupied': False, 'xl': 240, 'xr': 300},
-            {'occupied': False, 'xl': 420, 'xr': 480},
-            {'occupied': False, 'xl': 600, 'xr': 660},
-            {'occupied': False, 'xl': 780, 'xr': 840},
-        ]
-        self.scoring = [
-            {"visited": False, "value": 0},
-            {"visited": False, "value": 0},
-            {"visited": False, "value": 0},
-            {"visited": False, "value": 100},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 10},
-            {"visited": False, "value": 0},
-            {"visited": False, "value": 0},
+            {'occupied': False, 'xl': 50, 'xr': 100},
+            {'occupied': False, 'xl': 200, 'xr': 250},
+            {'occupied': False, 'xl': 350, 'xr': 400},
+            {'occupied': False, 'xl': 500, 'xr': 550},
+            {'occupied': False, 'xl': 650, 'xr': 750},
         ]
     
     def update(self):
-        # collision management is managed at the Game class level 
+        # delegate frog/object collision management to CollisionHandler class
         self.collision_handler.check_collisions(self.frog, self.level.objects)
 
-        # continual updating position of all game objects (vehicles, logs, turtles, etc.)
+        # update x-position of all game objects (vehicles, logs, turtles, etc.)
         for object in self.level.objects:
             if not self.screen.on_screen(object):
                 object.reset(self.screen)
             object.update()
         
-        # if frog y-position less than 180 (in row of home slots), check if frog within home boundaries
-
-        # continually update frog
+        # when frog dies, begin death_timer countdown
         if not self.frog.alive:
             if self.frog.death_timer > 0:
                 self.frog.death_timer -= 1
             else:
                 self.reset_frog()
+
+        # delegate scoring display management to Scoring class
+        self.scoring.update()
+
+        # delegate frog position management to Frog class
         self.frog.update()
 
-        # continually check to see if frog crosses screen boundaries
+        # update frog life status based on screen boundary collision
         if self.frog.alive and not self.screen.on_screen(self.frog):
             self.frog.die()
 
-    def points(self):
-        row = self.scoring[self.frog.rect.y // 50 - 1]
-        print(self.frog.rect.y // 50 - 1)
-        if not row["visited"]:
-            self.score += row["value"]
-            row["visited"] = True
+        # draw all game assets
+        self.draw()
 
     def draw(self):
+        # clear the screen
         self.screen.reset()
 
+        # draw all objects (vehicles, logs, turtles) based on level_map data file
         for object in self.level.objects:
             if object.image:
                 self.screen.surface.blit(object.image, (object.rect.x, object.rect.y))
-                    
+        
+        # if frog image is valid, draw frog image on the screen at frog.rect x, y position
         if self.frog.image:
             self.screen.surface.blit(self.frog.image, (self.frog.rect.x, self.frog.rect.y))
 
+        # draw small frog image below starting row for each frog live left
         for f in range(0, self.frog.lives):
             self.screen.surface.blit(self.frog.menu_image, (f * 60 + 10, 850 + self.screen.lane_padding))
 
+        # draw happy frog image in every home position that player has successfully reached
         for home in self.homes:
-            if home['occupied'] and self.frog.alive:
+            if home['occupied']:
                 self.screen.surface.blit(self.frog.image_home, (((home['xl'] + home['xr']) // 2 - self.frog.image_home.get_width() // 2), 124))
 
-        self.screen.score(self.score)
+        for home in self.homes:
+            print(home['occupied'])
+
+        # draw player display
+        self.screen.score(self.scoring.score)
+
+        draw_grid(self.screen)
+        
+        # flip the screen to display all of the above
         pygame.display.flip()
 
     def frog_in_home_row(self):

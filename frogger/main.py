c5e44cc15d963ef42ef7bd54aa5c1c7c86886753
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-18 09:55:31 -0700

    Removed points management from main.py. Added commenting.

--- frogger/main.py
+++ frogger/main.py
@@ -6,18 +6,20 @@ clock = pygame.time.Clock()
 
 def main():
     game = Game()
-    # create game loop
-    while not game.game_over():
+    # create game loop and run as long as player has frog lives remaining
+
+    while game.frog.lives:
         TimeManager.update(clock)
-        # capture event keyboard events and either quit game of pass to InputHandler
+
+        # capture keyboard events and either quit game or pass to InputHandler
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                game.lives = 0
+                game.frog.lives = 0
             elif game.frog.alive:
                 game.input_handler.handle_event(event)
-                game.points()
+        
+        # update game
         game.update()
-        game.draw()
 
 if __name__ == "__main__":
     main()

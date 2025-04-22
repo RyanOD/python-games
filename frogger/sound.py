113f455b63a5489605d57aed56fcb2ea865dc7d1
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-21 18:29:31 -0700

    Adding title_theme and main_theme as well as repeat field.

--- frogger/sound.py
+++ frogger/sound.py
@@ -7,18 +7,19 @@ class SoundHandler:
 
         # dict of all game sound files
         self.sounds = {
-            'hop': pygame.mixer.Sound('assets/hop.mp3'),
-            'die': pygame.mixer.Sound('assets/die.mp3')
-        }
+                'hop': {'mixer': pygame.mixer.Sound('assets/hop.mp3'), 'repeat': 0},
+                'die': {'mixer': pygame.mixer.Sound('assets/die.mp3'), 'repeat': 0},
+                'title_theme': {'mixer': pygame.mixer.Sound('assets/title_theme.mp3'), 'repeat': 0},
+                'main_theme': {'mixer': pygame.mixer.Sound('assets/main_theme.mp3'), 'repeat': -1}
+            }
 
         # set sound volumes
-        for sound in self.sounds.values():
-            sound.set_volume(0.5)
+        for key, value in self.sounds.items():
+            value['mixer'].set_volume(0.5)
 
         # pass trigger word and method for playing sound to event_dispatcher which allows other modules to trigger sounds
         event_dispatcher.register("play_sound", self.handle_sound)
 
     # method registered with and called from event_dispatcher
     def handle_sound(self, sound):
-        if(sound in self.sounds):
-            self.sounds[sound].play()
+        self.sounds[sound]['mixer'].play(loops=self.sounds[sound]['repeat'])

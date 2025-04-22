f3ae79cdad51ed681e4997e5ef9abbe6c08f0980
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-21 18:31:55 -0700

    Moving pygame.event into game.py. Moving calculation of dt into game.py.

--- frogger/main.py
+++ frogger/main.py
@@ -6,20 +6,19 @@ clock = pygame.time.Clock()
 
 def main():
     game = Game()
-    # create game loop and run as long as player has frog lives remaining
 
+    # create game loop and run as long as player has frog lives remaining
     while game.frog.lives:
         TimeManager.update(clock)
 
-        # capture keyboard events and either quit game or pass to InputHandler
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.frog.lives = 0
-            elif game.frog.alive:
-                game.input_handler.handle_event(event)
-        
         # update game
-        game.update()
+        game.update(TimeManager.get_delta_time())
+
+        # draw game assets
+        game.handle_input()
+
+        # draw game assets
+        game.draw()
 
 if __name__ == "__main__":
     main()

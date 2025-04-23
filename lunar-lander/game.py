dfcb8769b50cc6ec501c4b9bab44246767417888
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-23 16:30:24 -0700

    Added event management. Moved game play drawing to StatePlay.

--- lunar-lander/game.py
+++ lunar-lander/game.py
@@ -1,24 +1,44 @@
 import pygame
+import random
+from landscape_data import VERTICES, LANDINGS
+
 from ship import Ship
 from screen import Screen
 from config import SCREEN_HEIGHT, SCREEN_WIDTH, SHIP_HEIGHT, SHIP_WIDTH
 from input import InputHandler
+from state_machine import StateMachine
+from state_title import StateTitle
+from landscape import Landscape
 
 class Game:
     def __init__(self):
         pygame.init()  # Initialize Pygame
-        self.lives = 3
         self.ship = Ship()
+        self.landscape = Landscape()
         self.screen = Screen()
         self.input_handler = InputHandler(self.ship)
+        self.start_x = 10
+        self.start_y = 10
 
-    def update(self):
-        pass
+        # initialize state machine
+        self.state_machine = StateMachine()
+        self.state_machine.change_state(StateTitle(self))
 
+    def update(self, dt):
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.ship.lives = 0
+            elif self.ship.alive:
+                self.input_handler.handle_event(event)
+    
+        self.state_machine.update(dt, events)
+
+    def handle_input(self):
+        self.state_machine.handle_input()
+        
     def draw(self):
-        self.screen.draw()
-        self.screen.surface.blit(self.ship.image, (self.ship.x, self.ship.y))
-        pygame.display.flip()
+        self.state_machine.draw()
 
     def game_over(self):
-        return self.lives <= 0
+        return self.ship.lives <= 0

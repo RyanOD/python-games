131247dbb8e979c9884919a8e5f9ccba0dbe8315
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-24 10:48:02 -0700

    Passing in single dt value for use throughout program.

--- frogger/commands.py
+++ frogger/commands.py
@@ -5,29 +5,29 @@ class Command:
         pass
 
 class MoveUpCommand:
-    def __init__(self, frog):
+    def __init__(self, frog, dt):
         self.frog = frog
 
-    def execute(self):
-        self.frog.update("up")
+    def execute(self, dt):
+        self.frog.move(dt, "up")
 
 class MoveRightCommand:
-    def __init__(self, frog):
+    def __init__(self, frog, dt):
         self.frog = frog
 
-    def execute(self):
-        self.frog.update("right")
+    def execute(self, dt):
+        self.frog.move(dt, "right")
 
 class MoveDownCommand:
-    def __init__(self, frog):
+    def __init__(self, frog, dt):
         self.frog = frog
 
-    def execute(self):
-        self.frog.update("down")
+    def execute(self, dt):
+        self.frog.move(dt, "down")
 
 class MoveLeftCommand:
-    def __init__(self, frog):
+    def __init__(self, frog, dt):
         self.frog = frog
 
-    def execute(self):
-        self.frog.update("left")
+    def execute(self, dt):
+        self.frog.move(dt, "left")

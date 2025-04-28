8fd0b02c80a743f6c5ed59376a106d85336af1bd
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-04-28 14:58:11 -0700

    Add coin drop sound effect.

--- frogger/sound.py
+++ frogger/sound.py
@@ -10,7 +10,8 @@ class SoundHandler:
                 'hop': {'mixer': pygame.mixer.Sound('assets/hop.mp3'), 'repeat': 0},
                 'die': {'mixer': pygame.mixer.Sound('assets/die.mp3'), 'repeat': 0},
                 'title_theme': {'mixer': pygame.mixer.Sound('assets/title_theme.mp3'), 'repeat': 0},
-                'main_theme': {'mixer': pygame.mixer.Sound('assets/main_theme.mp3'), 'repeat': -1}
+                'main_theme': {'mixer': pygame.mixer.Sound('assets/main_theme.mp3'), 'repeat': -1},
+                'insert_coin': {'mixer': pygame.mixer.Sound('assets/insert_coin.mp3'), 'repeat': 0}
             }
 
         # set sound volumes

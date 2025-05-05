d93d192473e6d12cc3df421919a1a4e7da4c2130
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-05-05 12:49:43 -0700

    Updated passed arguments.

--- frogger/state_machine.py
+++ frogger/state_machine.py
@@ -1,3 +1,5 @@
+# Responsibility: Should handle input, because input behavior varies between states (e.g., Title screen vs. Game vs. Pause).
+
 class StateMachine():
     def __init__(self):
         self.current_state = None
@@ -8,13 +10,13 @@ class StateMachine():
         self.current_state = new_state
         self.current_state.enter()
 
-    def update(self, dt, events):
+    def update(self, dt = None, events = None):
         if self.current_state:
-            self.current_state.update(dt, events)
+            self.current_state.update(dt)
 
-    def handle_input(self):
+    def handle_input(self, dt, events):
         if self.current_state:
-            self.current_state.handle_input()
+            self.current_state.handle_input(dt, events)
 
     def draw(self):
         if self.current_state:

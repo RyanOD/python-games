71853e30cae035732289fc67d33e6d766dc796ac
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-05-12 10:57:52 -0700

    Can now handle more than one level.

--- frogger/utils.py
+++ frogger/utils.py
@@ -28,15 +28,15 @@ def get_image(images, image_type):
     return images[image_type]
 
 # create all game objects, store in objects array, and return the array
-def load_objects(level_map, images, object_factory):
+def load_objects(level_map, images, level, object_factory):
     objects = []
-    for level_data in level_map['levels']:
-        for row, lane_data in enumerate(level_data['lanes']):
-            for col, object_type in enumerate(lane_data['objects']):
-                if object_type:
-                    x = col * lane_data['width']
-                    y = row * lane_data['height']
-                    objects.append(object_factory(object_type, images, x, y, lane_data['movement']))
+    level_data = level_map['levels'][level - 1]
+    for row, lane_data in enumerate(level_data['lanes']):
+        for col, object_type in enumerate(lane_data['objects']):
+            if object_type:
+                x = col * lane_data['width']
+                y = row * lane_data['height']
+                objects.append(object_factory(object_type, images, x, y, lane_data['movement']))
     return objects
 
 # used to load background images for different game states

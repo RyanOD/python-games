331d4e916103af5a2fde9921d7b489a6c0f75e46
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-05-12 19:22:56 -0700

    Added drawing of level info text.

--- frogger/level.py
+++ frogger/level.py
@@ -1,15 +1,26 @@
 from objects import Object
 from utils import load_data_file, load_objects
+import pygame
 
 class Level:
     def __init__(self, game, images, level):
         self.game = game
-        self.level = level
         self.timer = 30
         self.level_map = load_data_file('level_data.json')
         self.images = images
-        self.objects = load_objects(self.level_map, self.images, self.level, Object)
-    
+        self.load_level(level)
+
     def update(self, dt):
         for object in self.objects:
             object.update(dt)
+    
+    def draw(self, screen, level):
+        font = pygame.font.Font("assets/upheavtt.ttf", 34)
+        level_title = pygame.font.Font.render(font, "Level", True, (255, 255, 255))
+        level = pygame.font.Font.render(font, str(level), False, (255, 255, 255))
+        screen.surface.blit(level_title, (300, 5))
+        screen.surface.blit(level, (300, 33))
+    
+    def load_level(self, level):
+        self.level = level
+        self.objects = load_objects(self.level_map, self.images, self.level, Object)

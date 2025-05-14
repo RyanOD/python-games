6d566dd1ee2b21396ed1f99836b45b202c88af78
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-05-14 11:16:12 -0700

    Reduce lives when frog enters home.

--- frogger/state_play.py
+++ frogger/state_play.py
@@ -47,6 +47,8 @@ class StatePlay(StateGame):
             if not self.game.homes[home_col]['occupied']:
                 event_dispatcher.dispatch('play_sound', 'landing_safe')
                 self.game.homes[home_col]['occupied'] = True
+                if not all(home['occupied'] for home in self.game.homes):
+                    self.game.frog.lives -= 1
                 self.game.scoring.reset_rows()
                 self.game.reset_level()
                 frog_reset(self.game.frog)

d6cf4a3bce1a45315b6c7f225c2357f0dc7d9a81
Ryan O'Donnell <odonnell.joseph@gmail.com>
2025-05-16 22:27:33 -0700

    Adjust layout and simplify choices.

--- frogger/state_menu.py
+++ frogger/state_menu.py
@@ -10,10 +10,7 @@ class StateMenu(StateGame):
         self.bg_image = get_bg_image("assets/menu_bg.png")
 
         # state specific attributes beyond background
-        self.play_buttons_beg = pygame.Rect(60, 540, 218, 70)
-        self.play_buttons_adv = pygame.Rect(475, 540, 218, 70)
-        self.rc_link = pygame.Rect(255, 710, 245, 15)
-        self.gh_link = pygame.Rect(260, 745, 234, 15)
+        self.play_button = pygame.Rect(268, 600, 218, 59)
         self.frog_img_1 = pygame.image.load('assets/frog_home_1.png')
 
     def enter(self):
@@ -30,20 +27,16 @@ class StateMenu(StateGame):
     def draw(self):
         self.game.screen.surface.fill((255, 255, 255))
         self.game.screen.draw(self.bg_image)
-        self.game.screen.surface.blit(self.frog_img_1, (SCREEN_WIDTH // 2 - 30, 545))
+        self.game.screen.surface.blit(self.frog_img_1, (SCREEN_WIDTH // 2 - 30, 500))
 
     def handle_input(self, dt = None, events = None):
-        for event in events: #look at all events
-            if event.type == pygame.QUIT:
-                self.frog.lives = 0
-            elif pygame.mouse.get_pressed()[0] == 1:
-                if self.play_buttons_beg.collidepoint(pygame.mouse.get_pos()) and self.coin_dropped == False:
+        for event in events:
+            if pygame.mouse.get_pressed()[0] == 1:
+                if self.play_button.collidepoint(pygame.mouse.get_pos()) and self.coin_dropped == False:
                     event_dispatcher.dispatch('play_sound', 'insert_coin')
                     self.frog_img_1 = pygame.image.load('assets/frog_home_2.png')
                     self.coin_dropped = True
                     self.coin_drop_timer += 1
-                elif self.play_buttons_adv.collidepoint(pygame.mouse.get_pos()) and self.coin_dropped == False:
-                    print("clicked - advanced")
 
     def exit(self):
         event_dispatcher.dispatch('stop_sound', 'insert_coin')
